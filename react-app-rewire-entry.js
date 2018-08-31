e1eac46c8bd5961580efabf0ba02ff872ca0ecb6
xiaosong <kunkkaco@gmail.com>
2018-08-31 09:41:22 +0800

    feat: react-app-rewire-entry

--- react-app-rewire-entry.js
+++ react-app-rewire-entry.js
@@ -1,111 +1,116 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const paths = require('react-scripts/config/paths');
 
-/**
- * TODO: 四种方式
- * 1. 数组方式 ['src/index.js', 'src\admin.js']
- * 2. 对象形式 { index: 'src/index.js', admin: 'src\admin.js' }
- * 3. 匹配目录下的文件 'src/pages/*.js'
- * 4. 匹配目录 'src\pages\*\index.js'
- */
+
 const rewireEntry = (entrys) => {
-  // TODO: 将四种格式的 entry 格式化成对象形式
-  const removePlugin = (plugins, nameMatcher) => {
-    return plugins.filter((it) => {
-      return !(it.constructor && it.constructor.name && nameMatcher(it.constructor.name));
-    });
-  };
-
-  const createHtmlWebpackPlugin = (entry, env) => {
-    const config = {
-      inject: true,
-      template: paths.appHtml,
-      // 多页配置：2.x 版本默认开启 optimization ，chunks 中需要增加 'vendors', 'runtime~admin'
-      chunks: ['vendors', `runtime~${entry}`, entry],
-      filename: `${entry}.html`,
-    };
-    if ('development' === env) {
-      return new HtmlWebpackPlugin(config);
-    } else {
-      return new HtmlWebpackPlugin({
-        ...config,
-        minify: {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-          keepClosingSlash: true,
-          minifyJS: true,
-          minifyCSS: true,
-          minifyURLs: true,
-        },
-      });
-    }
-  };
-
-  const getKeyByEntrySrc = (entrySrc) => {
-    const match = entrySrc.match(/^(.*[\/\\])?([^/.]+)\.[^.]+$/);
-    if (match) {
-      return match[2];
-    } else {
-      return 'index';
-    }
-  };
-
-  const rewireEntryConfig = (config, env) => {
-    const entryDependences = [require.resolve('react-scripts/config/polyfills')];
-    if ('development' !== env) {
-      entryDependences.push(require.resolve('react-dev-utils/webpackHotDevClient'));
-    }
-
-    const entryConfig = {};
-    for (let i = 0, l = entrys.length; i < l; i += 1) {
-      const key = getKeyByEntrySrc(entrys[i]);
-      entryConfig[key] = [...entryDependences, entrys[i]];
-    }
-    config.entry = entryConfig;
-    return config;
-  };
-
-  const rewireOutputConfig = (config, env) => {
-    if ('development' === env) {
-      config.output.filename = 'static/js/[name].bundle.js';
-    }
-    return config;
-  };
-
-  const rewireHtmlWebpackPlugin = (config, env) => {
-    config.plugins = removePlugin(config.plugins, (name) => /HtmlWebpackPlugin/i.test(name));
-
-    // const newPlugins = [];
-    for (let i = entrys.length - 1; 0 <= i; i -= 1) {
-      const key = getKeyByEntrySrc(entrys[i]);
-      const htmlPlugin = createHtmlWebpackPlugin(key, env);
-      config.plugins.unshift(htmlPlugin);
-    }
-    // config.plugins = replacePlugin(config.plugins, (name) => /HtmlWebpackPlugin/i.test(name), newPlugins);
-    return config;
-  };
+  const entry = getEntryObject(entrys);
 
   return {
     rewireWebpackEntryConfig: (config, env) => {
-      config = rewireEntryConfig(config, env);
+      config = rewireEntryConfig(entry, config, env);
       config = rewireOutputConfig(config, env);
-      config = rewireHtmlWebpackPlugin(config, env);
+      config = rewireHtmlWebpackPlugin(entry, config, env);
       return config;
     },
     rewireDevServerkEntryConfig: (config) => {
       const rewrites = [];
-      for (let i = 0, l = entrys.length; i < l; i += 1) {
-        const key = getKeyByEntrySrc(entrys[i]);
-        const reg = new RegExp(`^\\/${key}\\.html`);
-        rewrites.push({ from: reg, to: `/${paths.appBuild}/${key}.html` });
-      }
+      Object.keys(entry).map(name => {
+        const reg = new RegExp(`^\\/${name}\\.html`);
+        rewrites.push({ from: reg, to: `/${paths.appBuild}/${name}.html` });
+        return name;
+      });
+      config.historyApiFallback.rewrites = rewrites;
       return config;
     },
   };
+};
+
+const getEntryObject = (entry) => {
+  if (!(entry instanceof Array)) {
+    return entry;
+  }
+  const obj = {};
+  for (let i = 0, l = entry.length; i < l; i += 1) {
+    const name = getNameByEntrySrc(entry[i]);
+    obj[name] = entry[i];
+  }
+  return obj;
+};
+
+const getNameByEntrySrc = (entrySrc) => {
+  const match = entrySrc.match(/^(.*[\/\\])?([^/.]+)\.[^.]+$/);
+  if (match) {
+    return match[2];
+  } else {
+    return 'index';
+  }
+};
+
+const rewireEntryConfig = (entry, config, env) => {
+  const entryDependences = [require.resolve('react-scripts/config/polyfills')];
+  if ('development' !== env) {
+    entryDependences.push(require.resolve('react-dev-utils/webpackHotDevClient'));
+  }
+
+  const entryConfig = {};
+  Object.entries(entry).map(([name, entrySrc]) => {
+    entryConfig[name] = [...entryDependences, entrySrc];
+    return name;
+  });
+  config.entry = entryConfig;
+  return config;
+};
+
+const rewireOutputConfig = (config, env) => {
+  if ('development' === env) {
+    config.output.filename = 'static/js/[name].bundle.js';
+  }
+  return config;
+};
+
+const rewireHtmlWebpackPlugin = (entry, config, env) => {
+  config.plugins = removePlugin(config.plugins, (name) => /HtmlWebpackPlugin/i.test(name));
+
+  Object.keys(entry).map(name => {
+    const htmlPlugin = createHtmlWebpackPlugin(name, env);
+    config.plugins.unshift(htmlPlugin);
+    return name;
+  });
+  return config;
+};
+
+const removePlugin = (plugins, nameMatcher) => {
+  return plugins.filter((it) => {
+    return !(it.constructor && it.constructor.name && nameMatcher(it.constructor.name));
+  });
+};
+
+const createHtmlWebpackPlugin = (entryName, env) => {
+  const config = {
+    inject: true,
+    template: paths.appHtml,
+    chunks: ['vendors', `runtime~${entryName}`, entryName],
+    filename: `${entryName}.html`,
+  };
+  if ('development' === env) {
+    return new HtmlWebpackPlugin(config);
+  } else {
+    return new HtmlWebpackPlugin({
+      ...config,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    });
+  }
 };
 
 module.exports = rewireEntry;
